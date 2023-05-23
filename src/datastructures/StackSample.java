ce37e20c46a5233ac0ee4be43569488e93902f91
yujin <hsjes777@gmail.com>
2023-05-23 20:52:47 +0900

    stack

--- src/datastructures/StackSample.java
+++ src/datastructures/StackSample.java
@@ -1,2 +1,42 @@
-package datastructures;public class StackSample {
+package datastructures;
+
+import java.util.Stack;
+
+public class StackSample {
+    public static char[][] TOKENS = {{'{', '}'}, {'[', ']'}, {'(', ')'}};
+
+    public static boolean isOpenTerm(char c) {
+        for (char[] array : TOKENS) {
+            if (array[0] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean matches(char openTerm, char closeTerm) {
+        for (char[] array : TOKENS) {
+            if (array[0] == openTerm) {
+                return array[1] == closeTerm;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : expression.toCharArray()) {
+            if (isOpenTerm(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || !matches(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+
+    }
 }
