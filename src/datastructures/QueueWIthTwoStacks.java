36ed8e688404f23949e2c68bee4af3faa3a5cfc4
yujin <hsjes777@gmail.com>
2023-05-23 20:53:02 +0900

    queue with two stacks

--- src/datastructures/QueueWIthTwoStacks.java
+++ src/datastructures/QueueWIthTwoStacks.java
@@ -1,2 +1,32 @@
-package datastructures;public class QueueWIthTwoStacks {
+package datastructures;
+
+import java.util.Stack;
+
+public class QueueWIthTwoStacks {
+    public static class MyQueue<T> {
+        private Stack<T> stackNewestOnTop = new Stack<>();
+        private Stack<T> stackOldestOnTop = new Stack<>();
+
+        public void enqueue(T value) {
+            stackNewestOnTop.push(value);
+        }
+
+        public T peek() {
+            // move elements from stackNewest to stackOldest
+            shiftStacks();
+            return stackOldestOnTop.peek();
+        }
+
+        private void shiftStacks() {
+            if (stackOldestOnTop.isEmpty()) {
+                while (!stackNewestOnTop.isEmpty()) {
+                    stackOldestOnTop.push(stackNewestOnTop.pop());
+                }
+            }
+        }
+
+        public T dequeue() {
+            return stackNewestOnTop.pop();
+        }
+    }
 }
